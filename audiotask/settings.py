2ca8c2fcb5985aefb612aa7d5a70513c52b0ab47
shubham2200 <shubhambhalerao54312@gmail.com>
2022-05-09 09:47:12 +0530

    heroku

--- audiotask/settings.py
+++ audiotask/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)p6f_5fcuai#n!@y_k^zfqa##wf*(+g!cb4xew2_pt#f#@e^2%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["audio1234.herokuapp.com" , "localhost"]
 
 
 # Application definition
@@ -61,6 +61,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhitenoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -147,3 +148,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# https://audio1234.herokuapp.com/ | https://git.heroku.com/audio1234.git
+# set git remote heroku to https://git.heroku.com/audio1234.git
